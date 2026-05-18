57f0572748249416bb10979b0df07133cf356b51
Nicolasr-23 <rm93903@estudante.fieb.edu.br>
2026-05-18 10:43:31 -0300

    PDMO 1 COMMIT

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { WorkletsModule } from "react-native-worklets"
+import { Text, View } from "react-native";
+ 
+export default function Index() {
+  return (
+    <View>
+      <Text>Olá, Mundo!</Text>
+    </View>
+  );
+}
+ 
