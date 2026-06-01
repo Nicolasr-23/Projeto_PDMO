a10b7f5240fca1da44a7d9781960a269fc59c7ed
Nicolasr-23 <rm93903@estudante.fieb.edu.br>
2026-06-01 15:12:25 +0000

    formulário_copa_do_mundo

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,86 @@
-import { WorkletsModule } from "react-native-worklets"
-import { Text, View } from "react-native";
+import {
+  View,
+  Text,
+  TextInput,
+  Button,
+  Image,
+  StyleSheet
+} from "react-native";
  
-export default function Index() {
+export default function App() {
   return (
-    <View>
-      <Text>Olá, Mundo!</Text>
+    <View style={styles.container}>
+ 
+      <Text style={styles.titulo}>
+        Palpite da Copa do Mundo
+      </Text>
+ 
+      <Image
+        source={{
+          uri: "https://i.postimg.cc/1f0YMn9z/FIFA-Logo-2026.png"
+        }}
+        style={styles.imagem}
+      />
+ 
+      <Text style={styles.label}>
+        Seu nome:
+      </Text>
+ 
+      <TextInput
+        style={styles.input}
+        placeholder="Digite seu nome"
+      />
+ 
+      <Text style={styles.label}>
+        Qual seleção será campeã?
+      </Text>
+ 
+      <TextInput
+        style={styles.input}
+        placeholder="Digite a seleção"
+      />
+ 
+      <Button
+        title="Enviar palpite"
+        onPress={() => alert("Palpite enviado!")}
+      />
+ 
     </View>
   );
 }
  
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: 50,
+    padding: 20,
+  },
+ 
+  titulo: {
+    fontSize: 28,
+    fontWeight: "bold",
+    textAlign: "center",
+    marginBottom: 20,
+  },
+ 
+  imagem: {
+    width: 150,
+    height: 150,
+    alignSelf: "center",
+    marginBottom: 30,
+  },
+ 
+  label: {
+    fontSize: 18,
+    marginBottom: 8,
+  },
+ 
+  input: {
+    borderWidth: 1,
+    borderColor: "#999",
+    borderRadius: 8,
+    padding: 10,
+    marginBottom: 20,
+    fontSize: 16,
+  },
+});
